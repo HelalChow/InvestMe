944c33a845af1019aa94e7d1d84ea4d2227e9a0c
hchowdhury3273 <hc2324@nyu.edu>
2020-03-07 01:19:35 -0500

    navbar

--- investMe/investMe/CompanyViewController.swift
+++ investMe/investMe/CompanyViewController.swift
@@ -7,24 +7,93 @@
 //
 
 import UIKit
+import NotificationCenter
 
-class CompanyViewController: UIViewController {
+class CompanyViewController: UIViewController, UIGestureRecognizerDelegate {
+    
+    @IBOutlet weak var navBarView: UIView!
+    
+    
+    let closeThresholdHeight: CGFloat = 100
+    let openThreshold: CGFloat = UIScreen.main.bounds.height - 200
+    let closeThreshold = UIScreen.main.bounds.height - 100 // same value as closeThresholdHeight
+    var panGestureRecognizer: UIPanGestureRecognizer?
+    var animator: UIViewPropertyAnimator?
+
+    private var lockPan = false
 
     override func viewDidLoad() {
+        gotPanned(0)
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let gestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(respondToPanGesture))
+        view.addGestureRecognizer(gestureRecognizer)
+        gestureRecognizer.delegate = self
+        panGestureRecognizer = gestureRecognizer
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func gotPanned(_ percentage: Int) {
+        if animator == nil {
+            animator = UIViewPropertyAnimator(duration: 1, curve: .linear, animations: {
+                let scaleTransform = CGAffineTransform(scaleX: 1, y: 5).concatenating(CGAffineTransform(translationX: 0, y: 240))
+                self.navBarView.transform = scaleTransform
+                self.navBarView.alpha = 0
+            })
+            animator?.isReversed = true
+            animator?.startAnimation()
+            animator?.pauseAnimation()
+        }
+        animator?.fractionComplete = CGFloat(percentage) / 100
     }
-    */
 
+    // MARK: methods to make the view draggable
+
+    @objc func respondToPanGesture(recognizer: UIPanGestureRecognizer) {
+        guard !lockPan else { return }
+        if recognizer.state == .ended {
+            let maxY = UIScreen.main.bounds.height - CGFloat(openThreshold)
+            lockPan = true
+            if maxY > self.view.frame.minY {
+                maximize { self.lockPan = false }
+            } else {
+                minimize { self.lockPan = false }
+            }
+            return
+        }
+        let translation = recognizer.translation(in: self.view)
+        moveToY(self.view.frame.minY + translation.y)
+        recognizer.setTranslation(.zero, in: self.view)
+    }
+
+    func maximize(completion: (() -> Void)?) {
+        UIView.animate(withDuration: 0.2, animations: {
+            self.moveToY(0)
+        }) { _ in
+            if let completion = completion {
+                completion()
+            }
+        }
+    }
+
+    func minimize(completion: (() -> Void)?) {
+        UIView.animate(withDuration: 0.2, animations: {
+            self.moveToY(self.closeThreshold)
+        }) { _ in
+            if let completion = completion {
+                completion()
+            }
+        }
+    }
+
+    private func moveToY(_ position: CGFloat) {
+        view.frame = CGRect(x: 0, y: position, width: view.frame.width, height: view.frame.height)
+
+        let maxHeight = view.frame.height - closeThresholdHeight
+        let percentage = Int(100 - ((position * 100) / maxHeight))
+
+        gotPanned(percentage)
+
+        let name = NSNotification.Name(rawValue: "BottomViewMoved")
+        NotificationCenter.default.post(name: name, object: nil, userInfo: ["percentage": percentage])
+    }
 }
--- investMe/investMe/MapViewController.swift
+++ investMe/investMe/MapViewController.swift
@@ -7,24 +7,48 @@
 //
 
 import UIKit
+import MapKit
+import NotificationCenter
 
 class MapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: UIView!
+
+    var animator: UIViewPropertyAnimator?
+
+    func createBottomView() {
+        guard let sub = storyboard!.instantiateViewController(withIdentifier: "CompanyViewController") as? CompanyViewController else { return }
+        self.addChild(sub)
+        self.view.addSubview(sub.view)
+        sub.didMove(toParent: self)
+        sub.view.frame = CGRect(x: 0, y: view.frame.maxY, width: view.frame.width, height: view.frame.height)
+        sub.minimize(completion: nil)
+    }
+
+    func subViewGotPanned(_ percentage: Int) {
+        guard let propAnimator = animator else {
+            animator = UIViewPropertyAnimator(duration: 3, curve: .linear, animations: {
+                self.mapView.alpha = 1
+//                self.someView.transform = CGAffineTransform(scaleX: 0.8, y: 0.8).concatenating(CGAffineTransform(translationX: 0, y: -20))
+            })
+            animator?.startAnimation()
+            animator?.pauseAnimation()
+            return
+        }
+        propAnimator.fractionComplete = CGFloat(percentage) / 100
+    }
+
+    func receiveNotification(_ notification: Notification) {
+        guard let percentage = notification.userInfo?["percentage"] as? Int else { return }
+        subViewGotPanned(percentage)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        createBottomView()
 
-        // Do any additional setup after loading the view.
+        let name = NSNotification.Name(rawValue: "BottomViewMoved")
+        NotificationCenter.default.addObserver(forName: name, object: nil, queue: nil, using: receiveNotification(_:))
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
